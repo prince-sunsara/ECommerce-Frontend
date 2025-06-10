cbd07937d925f045de2e0bbfe353f57eee853f05
prince sunsara <princesunasara977@gmail.com>
2025-06-10 23:04:30 +0530

    Browser Feature

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,7 @@ import Navbar from "./layouts/Navbar";
 import { About, AboutUs, Contact, Home, Team } from "./pages";
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />}>
-            <Route path="about us" element={<AboutUs />} />
-            <Route path="team" element={<Team />} />
-            <Route path="contact" element={<Contact />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
