a1c53d85a4261c616972f34b18ec2471760e82bb
Mitesh-Amin01 <aminmiteshkumar.1484@gmail.com>
2025-06-10 23:03:02 +0530

    update app.jsx file and combine My And Prince Code

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,20 @@ import UserLogin from "./pages/LoginRegister/UserLogin";
 import SellerLogin from "./pages/SellerRegister/SellerLogin";
 import UserSignUp from "./pages/LoginRegister/UserSignUp";
 import SellerSignUp from "./pages/SellerRegister/SellerSignUp";
+import Navbar from "./layouts/Navbar";
+import { About, AboutUs, Contact, Home, Team } from "./pages";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />}>
+          <Route path="about us" element={<AboutUs />} />
+          <Route path="team" element={<Team />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
         <Route path="/user-login" element={<UserLogin />} />
         <Route path="/user-sign-up" element={<UserSignUp />} />
         <Route path="/seller-login" element={<SellerLogin />} />
