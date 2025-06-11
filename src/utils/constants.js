518b85adbdd22f7719dbe53f6c9e6a191950ffc5
asmitapatel192 <asmita.fosi@gmail.com>
2025-06-11 20:42:53 +0530

    Update Footer Design

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,22 @@
-export const COPY_RIGHT = "© 2025 MAJesto Team | ";
-export const FB_LINK = "https://www.facebook.com";
+// Website Copyright
+export const COPY_RIGHT = `© ${new Date().getFullYear()} Majesto Team. All rights reserved.`;
+
+// Social Media Links
+export const FB_LINK = "https://www.facebook.com/yourpage";
+export const INSTAGRAM_LINK = "https://www.instagram.com";
+export const TWITTER_LINK = "https://twitter.com/yourpage";
+export const PINTEREST_LINK = "https://www.pinterest.com/yourpage";
+
+// Company Contact Info
+export const COMPANY_PHONE = "(800) 555-0199";
+export const COMPANY_EMAIL = "majestoteam@.com";
+
+// Navigation Links
+export const NAV_LINKS = [
+  { path: "/about-us", label: "About Us" },
+  { path: "/contact-us", label: "Contact Us" },
+  { path: "/terms", label: "Terms & Conditions" },
+  { path: "/privacy", label: "Privacy Policy" },
+  { path: "/faq", label: "FAQ" },
+  { path: "/help-center", label: "Help Center" },
+];
